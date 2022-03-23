d34c589b53e62382bbbc826b4c5f8586e761e02b
John Waithira <waithirajon@gmail.com>
2022-03-23 19:32:38 +0300

    Open cv demo

---
+++ opencv.py
@@ -0,0 +1,25 @@
+"""
+MUST ROBOTICS
+Capturing video using Camera
+"""
+import cv2
+
+video = cv2.VideoCapture(0)
+
+if(video.isOpened() == False):
+    print("Failed")
+
+while(True):
+    ret, frame = video.read()
+    
+    cv2.imshow("Video Capturing", frame)
+    
+    if cv2.waitKey(1) & 0XFF ==ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
+
+
+#check the code from my github 
+
+# https://github.com/johnwaithira/opencv
